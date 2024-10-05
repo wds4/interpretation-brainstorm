b2ca4bdfeb71ae54bd94fa0d19310a905ac1f255
David Strayhorn <david.strayhorn4@gmail.com>
2024-10-05 13:48:11 -0400

    another bug fix

--- src/app_/api/requestInterpretation/route.ts
+++ src/app_/api/requestInterpretation/route.ts
@@ -4,11 +4,14 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const req = searchParams.get("req");
-  const oReq = JSON.parse(req)
   const client = await db.connect();
   try {
+    let oReq = {}
+    if (typeof req == 'string') {
+      oReq = JSON.parse(req)
+    }
     // const result = await client.sql`SELECT VERSION();`;
-    const response = oReq.a
+    const response = oReq.a || 'result not found'
     // return NextResponse.json({ result }, { status: 200 });
     return NextResponse.json( response, { status: 200 });
   } catch (error) {
