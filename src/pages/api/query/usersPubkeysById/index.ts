71be3822f855f4bbc5d3b88747d027667f85544c
David Strayhorn <david.strayhorn4@gmail.com>
2024-10-15 20:10:52 -0400

    update cron

--- src/pages/api/query/usersPubkeysById/index.ts
+++ src/pages/api/query/usersPubkeysById/index.ts
@@ -4,8 +4,8 @@ import { verifyPubkeyValidity } from '@/helpers/nip19';
 
 /*
 usage:
-http://localhost:3000/api/query/usersPubkeyById
-https://interpretation-brainstorm.vercel.app/api/query/usersPubkeyById
+http://localhost:3000/api/query/usersPubkeysById
+https://interpretation-brainstorm.vercel.app/api/query/usersPubkeysById
 
 returns an object used to fetch a user pubkey given the userID from the table: users
 
@@ -22,54 +22,25 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  const searchParams = req.query
-  if (searchParams.npub) {
-    // TODO: support npub
-  }
-  if (!searchParams.pubkey) {
-    const response:ResponseData = {
-      success: false,
-      message: 'api/query/usersPubkeyById: pubkey not provided',
-    }
-    res.status(500).json(response)
-  }
-  if (searchParams.pubkey) {
-    const pubkey1 = searchParams.pubkey
-    if ((typeof pubkey1 == 'string') && (verifyPubkeyValidity(pubkey1)) ) {
-      const client = await db.connect()
-      console.log('============ connecting the db client now')
-      try {
-        const resultUsers = await client.sql`SELECT id, pubkey FROM users`;
-        const resultUsersChars = JSON.stringify(resultUsers).length
-        const megabyteSize = resultUsersChars / 1048576
-    
-        const response: ResponseData = {
-          success: true,
-          message: 'Results of your usersPubkeyById query:',
-          data: {
-            megabyteSize
-          }
-        }
-        res.status(200).json(response)
-      } catch (e) {
-        console.log(e)
-      } finally {
-        console.log('============ releasing the db client')
-        client.release()
+  const client = await db.connect()
+  console.log('============ connecting the db client now')
+  try {
+    const resultUsers = await client.sql`SELECT id, pubkey FROM users`;
+    const resultUsersChars = JSON.stringify(resultUsers).length
+    const megabyteSize = resultUsersChars / 1048576
+
+    const response: ResponseData = {
+      success: true,
+      message: 'Results of your usersPubkeyById query:',
+      data: {
+        megabyteSize
       }
-    } else {
-      const response:ResponseData = {
-        success: false,
-        message: 'api/query/usersPubkeyById: invalid pubkey',
-      }
-      res.status(500).json(response)
     }
+    res.status(200).json(response)
+  } catch (e) {
+    console.log(e)
+  } finally {
+    console.log('============ releasing the db client')
+    client.release()
   }
-} 
-
-        /*
-
-                      [res10.rows[0].pubkey]: JSON.stringify(res10.rows[0].observerobject)
-
-
-        */
+}
